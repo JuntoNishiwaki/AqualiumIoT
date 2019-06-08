835f1d5ebceed71da2196972557871661a8859f8
JUNTO <junto918@gmail.com>
2019-06-08 12:39:59 +0900

    エラー処理

--- data_log.py
+++ data_log.py
@@ -331,7 +331,7 @@ try :
             # LCDへの出力
             print wtemp
             if wtemp is None:
-                wtemp = 25.0
+                break
             lcd.bme(wtemp,temp,humid,press,gas)
             df_write(dlm,gyo_m,'m',wtemp,temp,humid,press,gas)
             gyo_m += 1
