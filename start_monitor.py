3ec234fa1bfc82d5d686662cdd4767138d5da684
JUNTO <junto918@gmail.com>
2019-07-03 20:37:12 +0900

    エラー処理

--- start_monitor.py
+++ start_monitor.py
@@ -9,12 +9,12 @@ cmd3 = "python data_log_tmp.py"
 if __name__ == "__main__":
     print "START EBI COUNT"
     run1 =Popen(cmd1, shell=True)
-    time.sleep(1)
+    sleep(1)
     print "START FIG MAKE"
     run2 = Popen(cmd2, shell=True)
-    time.sleep(1)
+    sleep(1)
     print "START DATA LOG"
     run3= Popen(cmd2,shell=True)
     Popen.wait()
-    time.sleep(1)
+    sleep(1)
     print "End monitoring"
