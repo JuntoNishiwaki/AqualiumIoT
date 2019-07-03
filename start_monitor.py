22163eb73aafdd27ff5b151cbee53b18b668cb3a
JUNTO <junto918@gmail.com>
2019-07-03 20:19:07 +0900

    エラー処理

--- start_monitor.py
+++ start_monitor.py
@@ -1,32 +1,15 @@
 import subprocess
-from subprocess import Popen
-import threading
 from time import sleep
 
-cmd1 = "python ebi-countor.py"
-cmd2 = "python fig_make.py"
-cmd3 = "python data_log_tmp.py"
-
-def func1():
-        print ("Ebi coountor")
-        proc1 = Popen(cmd1, shell=True)
-        popen.wait()
-
-def func2():
-        print ("Fig make")
-        proc1 = Popen(cmd2, shell=True)
-        popen.wait()
-
-def func3():
-        print ("Data log")
-        proc1 = Popen(cmd2, shell=True)
-        popen.wait()
+cmd1 = "ebi-countor.py"
+cmd2 = "fig_make.py"
+cmd3 = "data_log_tmp.py"
 
 if __name__ == "__main__":
-    thread_1 = threading.Thread(target=func1)
-    thread_2 = threading.Thread(target=func2)
-    thread_3 = threading.Thread(target=func3)
-    
-    thread_1.start()
-    thread_2.start()
-    thread_3.start()
+    run1 = subprocess.call(["python" cmd1])
+    time.sleep(1)
+    run2 = subprocess.call(["python" cmd2])
+    time.sleep(1)
+    run3= subprocess.check_call(["python" cmd3])
+    time.sleep(1)
+    print "End monitoring"
