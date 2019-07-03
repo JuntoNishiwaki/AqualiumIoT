df472bcdb5d7c24e1bf582bb8d461e0022ba6ad4
JUNTO <junto918@gmail.com>
2019-07-03 20:22:20 +0900

    エラー処理

--- start_monitor.py
+++ start_monitor.py
@@ -6,10 +6,10 @@ cmd2 = "fig_make.py"
 cmd3 = "data_log_tmp.py"
 
 if __name__ == "__main__":
-    run1 = subprocess.call(["python" cmd1])
+    run1 = subprocess.call(["python",cmd1])
     time.sleep(1)
-    run2 = subprocess.call(["python" cmd2])
+    run2 = subprocess.call(["python",cmd2])
     time.sleep(1)
-    run3= subprocess.check_call(["python" cmd3])
+    run3= subprocess.check_call(["python",cmd3])
     time.sleep(1)
     print "End monitoring"
